a2ea1a5335e1ca44b598efb2ecdebb932fed5fa3
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-05-18 20:10:01 -1000

    ALL TESTS PASS

--- vault.js
+++ vault.js
@@ -5,13 +5,14 @@ module.exports = function() {
     var vault = {};
 
     function setValue(key, value){
-      vault.key = value;
-      return key;
+      // console.log(key, value);
+      vault[key] = value;
+      return vault[key];
     }
 
     function getValue(key){
       if (vault.hasOwnProperty(key)){
-        return vault.key;
+        return vault[key];
       } else {
         return null;
       }
